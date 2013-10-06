423ede646bb2a4e4485170c643f03e41005ef637
skreutzer <skreutzer@5b7c52e9-70e4-43ec-8a21-383427611a22>
2013-10-06 12:28:33 +0000

    Kleinere Korrekturen hinsichtlich verschobener Dateien/Verzeichnisse.

---
+++ ftp/gui/components/lang/en/navigation.lang.php
@@ -0,0 +1,33 @@
+<?php
+/* Copyright (C) 2013  Stephan Kreutzer
+ *
+ * This file is part of RPGBGPrototype.
+ *
+ * RPGBGPrototype is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3 or any later version,
+ * as published by the Free Software Foundation.
+ *
+ * RPGBGPrototype is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License 3 for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License 3
+ * along with RPGBGPrototype. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file $/gui/components/lang/en/navigation.lang.php
+ * @author Stephan Kreutzer
+ * @since 2013-10-06
+ */
+
+
+
+define("LANG_NAVIGATION_NORTH", "N");
+define("LANG_NAVIGATION_EAST", "E");
+define("LANG_NAVIGATION_SOUTH", "S");
+define("LANG_NAVIGATION_WEST", "W");
+
+
+
+?>
