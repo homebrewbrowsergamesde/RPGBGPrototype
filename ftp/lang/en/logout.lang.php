d068be30898a93507a014361afbd5d29ad4fef8a
skreutzer <skreutzer@5b7c52e9-70e4-43ec-8a21-383427611a22>
2013-10-06 13:30:07 +0000

    Logout hinzugefügt.

---
+++ ftp/lang/en/logout.lang.php
@@ -0,0 +1,32 @@
+<?php
+/* Copyright (C) 2013  Stephan Kreutzer
+ *
+ * This file is part of RPGBGPrototype.
+ *
+ * RPGBGPrototype is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3 or any later version,
+ * as published by the Free Software Foundation.
+ *
+ * RPGBGPrototype is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License 3 for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License 3
+ * along with RPGBGPrototype. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file $/lang/en/logout.lang.php
+ * @author Stephan Kreutzer
+ * @since 2013-10-06
+ */
+
+
+
+define("LANG_PAGETITLE", "RPGBGPrototype");
+define("LANG_LOGOUTSUCCESS", "Logout was successful.");
+define("LANG_MAINPAGEBUTTON", "Close");
+
+
+
+?>
--- ftp/logout.php
+++ ftp/logout.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2012  Stephan Kreutzer
+/* Copyright (C) 2012-2013  Stephan Kreutzer
  *
  * This file is part of RPGBGPrototype.
  *
@@ -23,6 +23,8 @@
 
 
 
+session_start();
+
 $_SESSION = array();
 
 if (isset($_COOKIE[session_name()]) == true)
@@ -30,23 +32,38 @@ if (isset($_COOKIE[session_name()]) == true)
     setcookie(session_name(), '', time()-42000, '/');
 }
 
+
+require_once("./libraries/languagelib.inc.php");
+require_once(getLanguageFile("logout"));
+
+
+
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n".
      "<!DOCTYPE html\n".
      "    PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"\n".
      "    \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n".
      "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">\n".
      "    <head>\n".
-     "        <title>RPGBGPrototype</title>\n".
-     "        <meta http-equiv=\"expires\" content=\"1296000\" />\n".
-     "        <meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\" />\n".
+     "        <title>".LANG_PAGETITLE."</title>\n".
+     "        <link rel=\"stylesheet\" type=\"text/css\" href=\"mainstyle.css\"/>\n".
+     "        <meta http-equiv=\"expires\" content=\"1296000\"/>\n".
+     "        <meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\"/>\n".
      "    </head>\n".
      "    <body>\n".
-     "        <div>\n".
-     "          Hier <a href=\"index.php\">weiter</a>.\n".
+     "        <div class=\"mainbox\">\n".
+     "          <div class=\"mainbox_body\">\n".
+     "            <p class=\"success\">\n".
+     "              ".LANG_LOGOUTSUCCESS."\n".
+     "            </p>\n".
+     "            <form action=\"index.php\" method=\"post\">\n".
+     "              <div>\n".
+     "                <input type=\"submit\" value=\"".LANG_MAINPAGEBUTTON."\"/><br/>\n".
+     "              </div>\n".
+     "            </form>\n".
+     "          </div>\n".
      "        </div>\n".
      "    </body>\n".
-     "</html>\n".
-     "\n";
+     "</html>\n";
 
 
 
